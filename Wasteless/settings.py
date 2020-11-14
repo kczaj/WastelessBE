c7bbab3f89c8993d973f0d9959199607f9cde5df
czajkaj <01142557@pw.edu.pl>
2020-11-14 17:21:01 +0100

    Poprawienie pliku settings.py

--- Wasteless/settings.py
+++ Wasteless/settings.py
@@ -76,9 +76,13 @@ WSGI_APPLICATION = 'Wasteless.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': "postgres",
+        'USER': 'postgres',
+        'PASSWORD': 'admin',
+        'HOST': '127.0.0.1',
+        'PORT': '5432',
+}
 }
 
 # Password validation
