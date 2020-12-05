df361564b5e883d761f461ff47333f46cc1a2173
redb3rry <michalmalinowski99@yahoo.co.uk>
2020-12-05 16:33:13 +0100

    Added fridge model and required relations. Changed quantity_g to float from int. WARNING: Running the app properly after merge will probably require running manage.py reset_database first, because of major changes to the db model.

--- api/serializers.py
+++ api/serializers.py
@@ -1,14 +1,14 @@
 from rest_framework import serializers
 from django.contrib.auth.models import User
-from .models import Product
+from .models import Product, Fridge
 
 
 class UserSerializer(serializers.HyperlinkedModelSerializer):
-    products = serializers.HyperlinkedRelatedField(many=True, view_name='product-detail', read_only=True)
+    fridges = serializers.HyperlinkedRelatedField(many=True, view_name='fridge-detail', read_only=True)
 
     class Meta:
         model = User
-        fields = ['id', 'username', 'products']
+        fields = ['id', 'username', 'fridges']
 
 
 class ProductSerializer(serializers.HyperlinkedModelSerializer):
@@ -16,5 +16,12 @@ class ProductSerializer(serializers.HyperlinkedModelSerializer):
     class Meta:
         model = Product
         fields = ('product_name', 'quantity_g', 'quantity', 'carbohydrates', 'energy_kcal', 'fat', 'fiber',
-                  'proteins', 'salt', 'sodium', 'image_url', 'date_added', 'expiration_date', 'fridge_id',
-                  'user_id')
+                  'proteins', 'salt', 'sodium', 'image_url', 'date_added', 'expiration_date', 'fridge_id')
+
+
+class FridgeSerializer(serializers.HyperlinkedModelSerializer):
+    products = serializers.HyperlinkedRelatedField(many=True, view_name='product-detail', read_only=True)
+
+    class Meta:
+        model = Fridge
+        fields = ('id', 'fridge_name', 'user_id', 'products')
--- api/views.py
+++ api/views.py
@@ -4,8 +4,8 @@ from rest_framework import viewsets, generics
 from rest_framework.permissions import IsAuthenticated, IsAdminUser
 from django.contrib.auth.models import User
 
-from .serializers import ProductSerializer, UserSerializer
-from .models import Product
+from .serializers import ProductSerializer, UserSerializer, FridgeSerializer
+from .models import Product, Fridge
 
 
 class UserViewSet(viewsets.ReadOnlyModelViewSet):
@@ -19,3 +19,8 @@ class ProductViewSet(viewsets.ModelViewSet):
     queryset = Product.objects.all().order_by('product_name')
     serializer_class = ProductSerializer
 
+
+class FridgeViewSet(viewsets.ModelViewSet):
+    permission_classes = (IsAuthenticated,)
+    queryset = Fridge.objects.all().order_by('fridge_name')
+    serializer_class = FridgeSerializer
