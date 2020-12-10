904c887b89b366c338b8f027c8cbe069ba6fb610
redb3rry <michalmalinowski99@yahoo.co.uk>
2020-12-10 17:14:24 +0100

    Changed to Token authentications (was session authentication previously).

--- api/urls.py
+++ api/urls.py
@@ -1,6 +1,8 @@
 from django.urls import include, path
 from rest_framework import routers
+from rest_framework.authtoken import views as auth_views
 from . import views
+
 
 router = routers.DefaultRouter()
 router.register(r'products', views.ProductViewSet)
@@ -12,6 +14,8 @@ router.register(r'fridges', views.FridgeViewSet)
 # Additionally, we include login URLs for the browsable API.
 urlpatterns = [
     path('', include(router.urls)),
-    path('api-auth/', include('rest_framework.urls', namespace='rest_framework')),
-    path('profile/', views.CurrentUserViewSet.as_view())
+    # path('api-auth/', include('rest_framework.urls', namespace='rest_framework')),
+    path('profile/', views.CurrentUserViewSet.as_view()),
+    path('login/', auth_views.obtain_auth_token, name='api-token-auth'),
+    path('logout/', views.Logout.as_view())
 ]
--- api/views.py
+++ api/views.py
@@ -1,8 +1,10 @@
 from django.shortcuts import render
 
-from rest_framework import viewsets, mixins, generics
+from rest_framework import viewsets, mixins, generics, status
 from rest_framework.permissions import IsAuthenticated, IsAdminUser
 from django.contrib.auth.models import User
+from rest_framework.views import APIView
+from rest_framework.response import Response
 
 from .serializers import ProductSerializer, UserSerializer, FridgeSerializer
 from .models import Product, Fridge
@@ -35,3 +37,9 @@ class FridgeViewSet(viewsets.ModelViewSet):
     permission_classes = (IsAuthenticated,)
     queryset = Fridge.objects.all().order_by('fridge_name')
     serializer_class = FridgeSerializer
+
+
+class Logout(APIView):
+    def get(self, request, format=None):
+        request.user.auth_token.delete()
+        return Response(status=status.HTTP_200_OK)
