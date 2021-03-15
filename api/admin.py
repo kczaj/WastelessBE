53623783f1b596469cdcf1a927c5ddc43a72dacf
redb3rry <michalmalinowski99@yahoo.co.uk>
2021-03-15 19:06:38 +0100

    Registered recipe and comment to admin panel

--- api/admin.py
+++ api/admin.py
@@ -1,5 +1,7 @@
 from django.contrib import admin
-from .models import Product, Fridge
+from .models import Product, Fridge, Recipe, Comment
 # Register your models here.
 admin.site.register(Product)
 admin.site.register(Fridge)
+admin.site.register(Recipe)
+admin.site.register(Comment)
