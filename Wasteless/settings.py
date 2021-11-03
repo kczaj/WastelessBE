1c03123ac678d07ed5114dffac2c916a9bdefa2c
Krisu17 <01142550@pw.edu.pl>
2021-11-03 13:13:58 +0100

    Added cors allow all origins to work with front

--- Wasteless/settings.py
+++ Wasteless/settings.py
@@ -145,6 +145,8 @@ django_heroku.settings(locals())
 
 # CORS
 
+CORS_ALLOW_ALL_ORIGINS = True # If this is used then `CORS_ALLOWED_ORIGINS` will not have any effect
+CORS_ALLOW_CREDENTIALS = True
 CORS_ALLOWED_ORIGINS = [
     'http://localhost:3000',
     'http://127.0.0.1:3000',
